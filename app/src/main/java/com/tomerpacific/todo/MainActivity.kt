fcaa1b891efa7a764a45d502cc9eb482514dfd01
unknown <tomerpacific@gmail.com>
2020-06-18 21:31:39 +0300

    Todo changing id for edittext and adding listener. Also added attribute to prevent soft keyboard from pushing layout up

--- app/src/main/java/com/tomerpacific/todo/MainActivity.kt
+++ app/src/main/java/com/tomerpacific/todo/MainActivity.kt
@@ -2,10 +2,13 @@ package com.tomerpacific.todo
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
 import android.widget.ListView
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,11 +16,23 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var list : ListView
+    private lateinit var title : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        title = findViewById(R.id.title)
+
+        title.setOnEditorActionListener {view, actionId, keyEvent ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH ||
+                keyEvent == null ||
+                keyEvent.keyCode == KeyEvent.KEYCODE_ENTER) {
+                true
+            }
+            false
+        }
 
         list = findViewById(R.id.todo_list)
         list.adapter = TodoListAdapter(this)
