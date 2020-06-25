e23136c6b3b96cd864d682972fe312fa7e47530a
unknown <tomerpacific@gmail.com>
2020-06-25 19:29:12 +0300

    Todo creating constants class

---
+++ app/src/main/java/com/tomerpacific/todo/TodoConstants.kt
@@ -0,0 +1,8 @@
+package com.tomerpacific.todo
+
+object TodoConstants {
+    const val ADD_NEW_TO_DO_ACTION = "addedNewTodo"
+    const val USER_NOTIFICATION_MISSING_TODO = "Please fill in a todo item"
+    const val TODO_SHARED_PREFERENCES_NAME = "todo_list_pref"
+    const val TODO_KEY = "todo"
+}
