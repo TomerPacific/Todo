85936931d59c3673a77bffc86cc25d9103f6d272
unknown <tomerpacific@gmail.com>
2020-07-07 21:07:02 +0300

    TOdo adding switch to UI to control where todo data is saved

--- app/src/main/java/com/tomerpacific/todo/LoginActivity.kt
+++ app/src/main/java/com/tomerpacific/todo/LoginActivity.kt
@@ -1,15 +1,24 @@
 package com.tomerpacific.todo
 
 import android.os.Bundle
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var switch : Switch
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         setSupportActionBar(toolbar)
+
+        switch = findViewById(R.id.switch1)
+
+        switch.setOnCheckedChangeListener {_, isChecked ->
+            switch.text = if (isChecked) "Online" else "On Device"
+        }
 
     }
 }
