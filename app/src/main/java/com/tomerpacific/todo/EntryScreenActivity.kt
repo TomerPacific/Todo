e92f17a522ed37d328fead80202eaf03901257e7
unknown <tomerpacific@gmail.com>
2020-07-10 09:23:01 +0300

    Todo logic to take into account the user's save preference

--- app/src/main/java/com/tomerpacific/todo/EntryScreenActivity.kt
+++ app/src/main/java/com/tomerpacific/todo/EntryScreenActivity.kt
@@ -10,11 +10,21 @@ class EntryScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val sharedPref = this.getSharedPreferences(TodoConstants.TODO_SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
+        val savingPreference = sharedPref.getString(TodoConstants.SAVE_DATA_PREFERENCE_KEY, "")
         val isFirstLogin = sharedPref.getBoolean(TodoConstants.FIRST_LOGIN_KEY, true)
-        val intent = when(isFirstLogin) {
-            true -> Intent(this, LoginActivity::class.java)
-            false -> Intent(this, MainActivity::class.java)
+
+        var intent : Intent?
+
+        if (isFirstLogin) {
+            intent = Intent(this, LoginActivity::class.java)
+        } else {
+            intent = when (savingPreference) {
+                TodoConstants.SAVE_DATA_ON_DEVICE -> Intent(this, MainActivity::class.java)
+                TodoConstants.SAVE_DATA_ONLINE -> Intent(this, LoginActivity::class.java)
+                else -> null
+            }
         }
+
         startActivity(intent)
         finish()
     }
