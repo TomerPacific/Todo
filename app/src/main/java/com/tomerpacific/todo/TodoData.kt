11811aab08eb2ad4015095e5824f5e167d788c02
unknown <tomerpacific@gmail.com>
2020-07-23 21:48:33 +0300

    Todo creating class for data received from BED

--- app/src/main/java/com/tomerpacific/todo/TodoData.kt
+++ app/src/main/java/com/tomerpacific/todo/TodoData.kt
@@ -1,2 +1,11 @@
 package com.tomerpacific.todo
 
+import com.google.gson.annotations.SerializedName
+
+class TodoData {
+    fun TodoData() {}
+
+    @SerializedName("todo_list")
+    var data : List<String> = listOf()
+
+}
