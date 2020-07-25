5dc1b20885af5c3b8a6e2c6ad9b417efee49e046
unknown <tomerpacific@gmail.com>
2020-07-25 20:55:46 +0300

    Todo creating classes for setting todo data in the backedn

--- app/src/main/java/com/tomerpacific/todo/TodoDataSet.kt
+++ app/src/main/java/com/tomerpacific/todo/TodoDataSet.kt
@@ -1,2 +1,15 @@
 package com.tomerpacific.todo
 
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class TodoDataSet(_username: String?, _data : List<String>) {
+
+    @SerializedName("username")
+    @Expose
+    var username: String? = _username
+    @SerializedName("data")
+    @Expose
+    var data: List<String>? = _data
+
+}
--- app/src/main/java/com/tomerpacific/todo/TodoDataSetResult.kt
+++ app/src/main/java/com/tomerpacific/todo/TodoDataSetResult.kt
@@ -1,2 +1,9 @@
 package com.tomerpacific.todo
 
+import com.google.gson.annotations.SerializedName
+
+class TodoDataSetResult {
+
+    @SerializedName("message")
+    var message : String = ""
+}
