aa309d32de378f3438c371232f5f7fc9d586ab88
unknown <tomerpacific@gmail.com>
2020-07-27 22:24:50 +0300

    Todo adding method to remove all todo items to DatabaseSaving service

--- app/src/main/java/com/tomerpacific/todo/services/TodoDatabaseService.kt
+++ app/src/main/java/com/tomerpacific/todo/services/TodoDatabaseService.kt
@@ -1,2 +1,17 @@
 package com.tomerpacific.todo.services
 
+class TodoDatabaseService private constructor() {
+
+
+    private object HOLDER {
+        val INSTANCE = TodoDatabaseService()
+    }
+
+    companion object {
+        val instance: TodoDatabaseService by lazy { HOLDER.INSTANCE }
+    }
+
+    fun removeAllTodos() {
+
+    }
+}
