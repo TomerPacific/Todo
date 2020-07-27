add6a693d2b21192d353d7f35b7335bedfe8a797
unknown <tomerpacific@gmail.com>
2020-07-27 22:22:32 +0300

    Todo moving logic on which data saving methods to enact to manager

--- app/src/main/java/com/tomerpacific/todo/DataSavingManager.kt
+++ app/src/main/java/com/tomerpacific/todo/DataSavingManager.kt
@@ -1,2 +1,38 @@
 package com.tomerpacific.todo
 
+import android.content.Context
+import com.tomerpacific.todo.services.TodoDataSharedPreferencesService
+import com.tomerpacific.todo.services.TodoDatabaseService
+
+object DataSavingManager {
+
+    private var shouldSaveInSharedPreferences : Boolean? = null
+
+    fun decideOnUserDataSavingFlow(context: Context) {
+        val sharedPref = context.getSharedPreferences(
+            TodoConstants.TODO_SHARED_PREFERENCES_NAME,
+            Context.MODE_PRIVATE
+        )
+
+        val shouldSaveDataInSharedPreferences: String? = sharedPref.getString(
+            TodoConstants.SAVE_DATA_PREFERENCE_KEY,
+            TodoConstants.SAVE_DATA_ON_DEVICE
+        )
+
+        shouldSaveInSharedPreferences =
+            when(shouldSaveDataInSharedPreferences) {
+                TodoConstants.SAVE_DATA_ON_DEVICE -> true
+                else -> false
+        }
+    }
+
+    fun removeAllTodoData() {
+        when (shouldSaveInSharedPreferences != null) {
+            true -> TodoDataSharedPreferencesService.instance.removeAllTodos()
+            false -> TodoDatabaseService.instance.removeAllTodos()
+        }
+
+    }
+
+
+}
