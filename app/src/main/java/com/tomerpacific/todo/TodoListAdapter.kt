7e8d6d1ddd74ee2a69ec530fc84de5eea359e992
unknown <tomerpacific@gmail.com>
2020-08-13 23:11:50 +0300

    Todo removung unused imports

--- app/src/main/java/com/tomerpacific/todo/TodoListAdapter.kt
+++ app/src/main/java/com/tomerpacific/todo/TodoListAdapter.kt
@@ -1,10 +1,6 @@
 package com.tomerpacific.todo
 
-
 import android.content.Context
-import android.graphics.Color
-import android.graphics.PorterDuff
-import android.graphics.PorterDuffColorFilter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
