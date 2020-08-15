0457231f1c119dfd7fcdf0162652b589cfc109e0
unknown <tomerpacific@gmail.com>
2020-08-15 12:59:18 +0300

    Todo refactoring EntryScreenActivity

--- app/src/main/java/com/tomerpacific/todo/activities/EntryScreenActivity.kt
+++ app/src/main/java/com/tomerpacific/todo/activities/EntryScreenActivity.kt
@@ -12,22 +12,19 @@ class EntryScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val sharedPref = this.getSharedPreferences(TodoConstants.TODO_SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
         val savingPreference = sharedPref.getString(TodoConstants.SAVE_DATA_PREFERENCE_KEY, "")
-        val isFirstLogin = sharedPref.getBoolean(TodoConstants.FIRST_LOGIN_KEY, true)
 
-        var intent : Intent?
-
-        if (isFirstLogin) {
-            intent = Intent(this, DataSavePreferenceActivity::class.java)
-        } else {
-            intent = when (savingPreference) {
+        val intent : Intent = when(sharedPref.getBoolean(TodoConstants.FIRST_LOGIN_KEY, true)) {
+            true -> Intent(this, DataSavePreferenceActivity::class.java)
+            false -> when (savingPreference) {
                 TodoConstants.SAVE_DATA_ON_DEVICE -> Intent(this, MainActivity::class.java)
                 TodoConstants.SAVE_DATA_ONLINE -> Intent(this, LoginActivity::class.java)
-                else -> null
+                else -> Intent(this, MainActivity::class.java)
             }
         }
 
         startActivity(intent)
         finish()
+
     }
 
 }
