817c754e7c5ae9413629ca94f7b76219e4b24303
unknown <tomerpacific@gmail.com>
2020-09-28 13:40:49 +0300

    Todo converting getUserIdGToken to use a coroutine and become a suspend function. Altered removeAllTodos to use it

--- app/src/main/java/com/tomerpacific/todo/services/TodoDatabaseService.kt
+++ app/src/main/java/com/tomerpacific/todo/services/TodoDatabaseService.kt
@@ -1,8 +1,10 @@
 package com.tomerpacific.todo.services
 
 import android.content.Context
+import kotlinx.coroutines.*
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.tomerpacific.todo.data.TodoData
 import com.tomerpacific.todo.data.TodoDataSetResult
 import com.tomerpacific.todo.TodoConstants
@@ -99,47 +101,51 @@ class TodoDatabaseService private constructor() {
         }
     }
 
-    fun removeAllTodos(context: Context) {
+    fun removeAllTodos(context: Context) = runBlocking {
 
-        val user = FirebaseAuth.getInstance().currentUser
+        val user : FirebaseUser? = FirebaseAuth.getInstance().currentUser
 
         if (user != null) {
-            user.getIdToken(false).addOnCompleteListener {
-                if (it.isSuccessful) {
-                    val token = it.result?.token
+            val idToken = getUserIdToken(user)
 
-                    val retrofit = Retrofit.Builder()
-                        .baseUrl(TodoConstants.BASE_URL_FOR_REQUEST)
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build()
-                    val service = retrofit.create(DataService::class.java)
-                    val call = service.removeAllData(token, getUserUUID())
+            val retrofit = Retrofit.Builder()
+                .baseUrl(TodoConstants.BASE_URL_FOR_REQUEST)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            val service = retrofit.create(DataService::class.java)
+            val call = service.removeAllData(idToken, getUserUUID())
 
-                    call.enqueue(object : Callback<TodoDataSetResult> {
-                        override fun onResponse(
-                            call: Call<TodoDataSetResult>,
-                            response: Response<TodoDataSetResult>
-                        ) {
-                            if (!response.isSuccessful) {
-                                Toast.makeText(
-                                    context,
-                                    "There was a problem saving the Todo data in the server",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                        }
-
-                        override fun onFailure(call: Call<TodoDataSetResult>, t: Throwable) {
-                            Toast.makeText(
-                                context,
-                                "There was a problem saving the Todo data in the server",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                    })
+            call.enqueue(object : Callback<TodoDataSetResult> {
+                override fun onResponse(
+                    call: Call<TodoDataSetResult>,
+                    response: Response<TodoDataSetResult>
+                ) {
+                    if (!response.isSuccessful) {
+                        Toast.makeText(
+                            context,
+                            "There was a problem saving the Todo data in the server",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
-            }
+
+                override fun onFailure(call: Call<TodoDataSetResult>, t: Throwable) {
+                    Toast.makeText(
+                        context,
+                        "There was a problem saving the Todo data in the server",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
         }
+    }
+
+    private suspend fun getUserIdToken(user: FirebaseUser) = coroutineScope {
+
+        val job = async {
+            user.getIdToken(false).result?.token
+        }
+        job.await()
     }
 
     private fun getUserUUID() : String {
