a5ffce93f737583ea985dbd5b72346d6782a48a6
unknown <tomerpacific@gmail.com>
2020-10-30 23:56:03 +0200

    Todo adding application context and changing inhertiance

--- app/src/main/java/com/tomerpacific/todo/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/com/tomerpacific/todo/viewmodels/MainActivityViewModel.kt
@@ -1,11 +1,22 @@
 package com.tomerpacific.todo.viewmodels
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.tomerpacific.todo.models.TodoData
+import com.tomerpacific.todo.repositories.TodoRepository
 
-class MainActivityViewModel : ViewModel() {
+class MainActivityViewModel(application: Application) : AndroidViewModel(application) {
 
     private var mTodoData : MutableLiveData<List<TodoData>> = MutableLiveData()
+
+    init {
+        mTodoData = TodoRepository.getTodoData(application.applicationContext)
+    }
+
+    fun getTodoData() : LiveData<List<TodoData>> {
+        return mTodoData
+    }
 
 }
