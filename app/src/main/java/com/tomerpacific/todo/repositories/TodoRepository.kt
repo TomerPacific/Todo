d3f6945012083d542c943f8332fe7ce92b31562f
unknown <tomerpacific@gmail.com>
2020-10-31 21:36:14 +0200

    Todo logic to fetch data from DataSavingManager

--- app/src/main/java/com/tomerpacific/todo/repositories/TodoRepository.kt
+++ app/src/main/java/com/tomerpacific/todo/repositories/TodoRepository.kt
@@ -1,17 +1,24 @@
 package com.tomerpacific.todo.repositories
 
+import android.content.Context
 import androidx.lifecycle.MutableLiveData
+import com.tomerpacific.todo.DataSavingManager
 import com.tomerpacific.todo.models.TodoData
 
 object TodoRepository {
 
     private var todoData : ArrayList<TodoData> = ArrayList()
 
-    fun getTodoData(): MutableLiveData<List<TodoData>> {
+    fun getTodoData(context: Context): MutableLiveData<List<TodoData>> {
+        setTodoData(context)
         var data = MutableLiveData<List<TodoData>>()
         data.value = todoData
 
         return data
+    }
+
+    private fun setTodoData(context: Context) {
+        DataSavingManager.fetchTodoData(context)
     }
 
 }
