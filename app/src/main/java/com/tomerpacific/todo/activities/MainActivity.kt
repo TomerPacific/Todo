c5f7a6c327d28bc6466d10106e420f01b6587228
unknown <tomerpacific@gmail.com>
2020-11-14 20:26:07 +0200

    Todo adding listener to set positive alert dialog button to be enabled

--- app/src/main/java/com/tomerpacific/todo/activities/MainActivity.kt
+++ app/src/main/java/com/tomerpacific/todo/activities/MainActivity.kt
@@ -138,7 +138,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             builder.setNegativeButton("No"){ dialogInterface, which ->
-                
+
             }
 
             val alertDialog: AlertDialog = builder.create()
@@ -146,6 +146,24 @@ class MainActivity : AppCompatActivity() {
             alertDialog.show()
 
             alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = false
+
+            userInputEditText.apply {
+                addTextChangedListener (object : TextWatcher {
+                    override fun afterTextChanged(p0: Editable?) {
+                        if (!p0.isNullOrEmpty()) {
+                            alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = true
+                        }
+                    }
+
+                    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                    }
+
+                    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                    }
+                })
+            }
 
         }
 
