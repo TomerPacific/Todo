e69846f2843b1afdb40061cef1f5fbb22aabf229
unknown <tomerpacific@gmail.com>
2022-09-22 23:41:32 +0300

    Todo changing backend endpoint

--- app/src/main/java/com/tomerpacific/todo/TodoConstants.kt
+++ app/src/main/java/com/tomerpacific/todo/TodoConstants.kt
@@ -8,7 +8,7 @@ object TodoConstants {
     const val SAVE_DATA_ON_DEVICE = "On Device"
     const val SAVE_DATA_ONLINE = "Online"
     const val SAVE_DATA_PREFERENCE_KEY = "savePreference"
-    const val BASE_URL_FOR_REQUEST = "https://todo-tomer.herokuapp.com/"
+    const val BASE_URL_FOR_REQUEST = "https://todo-server-zx38.onrender.com/"
     const val TODO_ACTION_NEW_TODO_ITEM = "NEW_TODO_ITEM"
     const val WHATSAPP_PACKAGE = "com.whatsapp"
 }
