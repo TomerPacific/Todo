fe9054518c6cd040b4bf5fadb69a116034ab22fa
unknown <tomerpacific@gmail.com>
2023-12-01 22:06:20 +0200

    Todo creating login screen

--- app/src/main/java/com/tomerpacific/todo/LoginScreenCompose.kt
+++ app/src/main/java/com/tomerpacific/todo/LoginScreenCompose.kt
@@ -1,4 +1,28 @@
 package com.tomerpacific.todo
 
-class LoginScreenCompose {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
+@Composable
+fun LoginScreenCompose() {
+    val email = remember {
+        mutableStateOf("")
+    }
+
+    Column() {
+        Row() {
+            TextField(
+                value = email.value,
+                onValueChange = { it: String -> email.value = it },
+                label = {
+                    Text("Email")
+                }
+            )
+        }
+    }
 }
