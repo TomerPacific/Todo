65f80c281435c5b5878c562371acbfc17adf5313
unknown <tomerpacific@gmail.com>
2023-12-05 16:31:44 +0200

    Todo logic to read/write from datastore

---
+++ app/src/main/java/service/TodoItemsRepository.kt
@@ -0,0 +1,26 @@
+package service
+
+import androidx.datastore.core.DataStore
+import com.tomerpacific.todo.TodoItem
+import com.tomerpacific.todo.TodoItems
+import kotlinx.coroutines.flow.catch
+import java.io.IOException
+
+class TodoItemsRepository(private val todoItemsDataStore: DataStore<TodoItems>) {
+
+    val todoItemsFlow = todoItemsDataStore.data
+        .catch { exception ->
+            if (exception is IOException) {
+                emit(TodoItems.getDefaultInstance())
+            } else {
+                throw exception
+            }
+        }
+
+    suspend fun updateTodoItems(todoItems: List<TodoItem>) {
+        todoItemsDataStore.updateData {
+            it.toBuilder().clearItems().addAllItems(todoItems).build()
+        }
+    }
+
+}
