1e4b4375e979eab54152dc028fe6463c07d9a937
unknown <tomerpacific@gmail.com>
2023-12-08 19:56:40 +0200

    Todo creating model for todo item

--- app/src/main/java/model/TodoItem.kt
+++ app/src/main/java/model/TodoItem.kt
@@ -1,3 +1,15 @@
 package model
 
-data class TodoItem()
+import java.util.UUID
+
+data class TodoItem(
+    val itemId: String = generateItemId(),
+    var itemDescription: String
+) {
+    companion object {
+
+        private fun generateItemId(): String {
+            return UUID.randomUUID().toString()
+        }
+    }
+}
