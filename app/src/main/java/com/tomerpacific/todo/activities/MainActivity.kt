3ffa8b0b1813d20aa319754a4a64120ae68dd5ea
unknown <tomerpacific@gmail.com>
2023-12-08 15:16:38 +0200

    Todo adding fab

--- app/src/main/java/com/tomerpacific/todo/activities/MainActivity.kt
+++ app/src/main/java/com/tomerpacific/todo/activities/MainActivity.kt
@@ -7,15 +7,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
@@ -42,35 +46,47 @@ class MainActivity: AppCompatActivity() {
             val todoItems = mainViewModel.todoItems.observeAsState()
 
             MaterialTheme {
-                Column(modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally) {
-                    Row(modifier = Modifier.fillMaxSize(),
-                    horizontalArrangement = Arrangement.Center) {
-                        OutlinedTextField(
-                            textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center),
-                            value = todoListTitle.value,
-                            onValueChange = {
-                                todoListTitle.value = it
-                        },
-                        colors = OutlinedTextFieldDefaults.colors(
-                            focusedBorderColor = Color.Black,
-                            unfocusedBorderColor = Color.Transparent,
-                            ),
-                            trailingIcon = {
-                                Icon(Icons.Default.Edit, contentDescription = "Edit Icon")
-                            }
-                        )
+                Scaffold(floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = {  },
+                        containerColor = Color.Black,
+                        contentColor = Color.White
+                    ) {
+                        Icon(Icons.Filled.Add, "Floating action button.")
                     }
 
-                    if (todoItems.value != null) {
-                        LazyColumn() {
-                            items(todoItems.value!!.itemsList) { todoItem ->
-                                Text(todoItem.itemDescription)
+                }) { paddingValues ->
+                    Column(modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally) {
+                        Row(modifier = Modifier.fillMaxSize(),
+                            horizontalArrangement = Arrangement.Center) {
+                            OutlinedTextField(
+                                textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center),
+                                value = todoListTitle.value,
+                                onValueChange = {
+                                    todoListTitle.value = it
+                                },
+                                colors = OutlinedTextFieldDefaults.colors(
+                                    focusedBorderColor = Color.Black,
+                                    unfocusedBorderColor = Color.Transparent,
+                                ),
+                                trailingIcon = {
+                                    Icon(Icons.Default.Edit, contentDescription = "Edit Icon")
+                                }
+                            )
+                        }
+
+                        if (todoItems.value != null) {
+                            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                                items(todoItems.value!!.itemsList) { todoItem ->
+                                    Text(todoItem.itemDescription)
+                                }
                             }
                         }
                     }
-
                 }
             }
         }
