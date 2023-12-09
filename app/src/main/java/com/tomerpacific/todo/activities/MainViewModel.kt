2d63dd614287551180965f077034a8d9baf4185c
unknown <tomerpacific@gmail.com>
2023-12-09 14:58:04 +0200

    Todo logic to update datastore when new todo item is created

--- app/src/main/java/com/tomerpacific/todo/activities/MainViewModel.kt
+++ app/src/main/java/com/tomerpacific/todo/activities/MainViewModel.kt
@@ -8,10 +8,12 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.tomerpacific.todo.TodoItem
 import com.tomerpacific.todo.TodoItems
 import kotlinx.coroutines.launch
 import service.TodoItemsRepository
 import service.TodoItemsSerializer
+import java.util.UUID
 
 
 private const val DATA_STORE_FILE_NAME = "todoItemsList.proto"
@@ -30,9 +32,21 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
     init {
         getTodoItems()
     }
+
     private fun getTodoItems() {
         viewModelScope.launch {
             _todoItems.value = todoItemsRepository.fetchCachedTodoItems()
+        }
+    }
+
+    fun addTodoItem(todoItemDescription: String) {
+        val todoItem = TodoItem.newBuilder().setItemId(UUID.randomUUID().toString())
+            .setItemDescription(todoItemDescription).build()
+        _todoItems.value?.let {
+            it.itemsList.add(todoItem)
+            viewModelScope.launch {
+                todoItemsRepository.updateTodoItems(it.itemsList)
+            }
         }
     }
 
