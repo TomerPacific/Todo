4805438770402ad908bf9f99f436c4ae1b37ac0f
unknown <tomerpacific@gmail.com>
2023-12-14 19:43:36 +0200

    Todo adding completion icon and coloring icons appropriately

--- app/src/main/java/com/tomerpacific/todo/activities/TodoItemView.kt
+++ app/src/main/java/com/tomerpacific/todo/activities/TodoItemView.kt
@@ -1,11 +1,13 @@
 package com.tomerpacific.todo.activities
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
@@ -14,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.tomerpacific.todo.TodoItem
@@ -32,8 +35,20 @@ fun TodoItemView(todoItem: TodoItem, viewModel: MainViewModel) {
                 modifier = Modifier.fillMaxHeight(),
                 onClick = {
                     viewModel.removeTodoItem(todoItem)
+                }) {
+                Icon(Icons.Default.Check,
+                    "Complete Todo",
+                    tint = Color.Green)
+            }
+            IconButton(
+                modifier = Modifier.fillMaxHeight(),
+                onClick = {
+                    viewModel.removeTodoItem(todoItem)
             }) {
-                Icon(Icons.Default.Close, "Delete Todo")
+                Icon(
+                    Icons.Default.Close,
+                    "Delete Todo",
+                   tint = Color.Red)
             }
         }
     }
