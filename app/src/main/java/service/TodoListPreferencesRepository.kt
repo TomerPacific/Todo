05c8801768dc63bb5ad238eb18d7fbaa7b594c23
unknown <tomerpacific@gmail.com>
2023-12-15 22:50:36 +0200

    Todo creating repository class for new type

---
+++ app/src/main/java/service/TodoListPreferencesRepository.kt
@@ -0,0 +1,24 @@
+package service
+
+import androidx.datastore.core.DataStore
+import com.tomerpacific.todo.TodoListPreferences
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.first
+import java.io.IOException
+
+
+class TodoListPreferencesRepository(private val todoListPreferencesDataStore: DataStore<TodoListPreferences>) {
+
+    val todoListPreferencesFlow: Flow<TodoListPreferences> = todoListPreferencesDataStore.data
+        .catch { exception ->
+            if (exception is IOException) {
+                emit(TodoListPreferences.getDefaultInstance())
+            } else {
+                throw exception
+            }
+        }
+
+    suspend fun fetchCachedTodoListPreferences() = todoListPreferencesFlow.first()
+
+}
