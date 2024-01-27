05a4659d99e91b057949b8aa06e90db7bbce9a55
unknown <tomerpacific@gmail.com>
2024-01-27 10:37:20 +0200

    Todo removing unused import

--- app/src/main/java/com/tomerpacific/todo/view/MainActivity.kt
+++ app/src/main/java/com/tomerpacific/todo/view/MainActivity.kt
@@ -20,7 +20,6 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
-import androidx.compose.material3.DismissState
 import androidx.compose.material3.DismissValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
