c596506b756ffde4cda1667f6d5f94dd25870447
unknown <tomerpacific@gmail.com>
2024-02-09 21:23:22 +0200

    Todo creating TodoState class

---
+++ app/src/main/java/com/tomerpacific/todo/view/TodoState.kt
@@ -0,0 +1,9 @@
+package com.tomerpacific.todo.view
+
+import com.tomerpacific.todo.TodoItem
+
+data class TodoState(
+    val todoItems: List<TodoItem> = emptyList(),
+    val todoItemDescription: String = "",
+    val isAddingTodo: Boolean = false
+)
