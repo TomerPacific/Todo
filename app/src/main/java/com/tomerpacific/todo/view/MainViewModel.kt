78db3a5983b7231a457acbbe9912f2a4dd1f069c
unknown <tomerpacific@gmail.com>
2024-02-10 19:12:15 +0200

    Todo adding onEvent logic

--- app/src/main/java/com/tomerpacific/todo/view/MainViewModel.kt
+++ app/src/main/java/com/tomerpacific/todo/view/MainViewModel.kt
@@ -11,6 +11,8 @@ import androidx.lifecycle.viewModelScope
 import com.tomerpacific.todo.TodoItem
 import com.tomerpacific.todo.TodoItems
 import com.tomerpacific.todo.TodoListPreferences
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import service.TodoItemsRepository
 import service.TodoItemsSerializer
@@ -34,9 +36,51 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
     val todoItemsFlow: LiveData<TodoItems> = todoItemsRepository.todoItemsFlow.asLiveData()
 
     val todoListPreferencesFlow: LiveData<TodoListPreferences> = todoListPreferencesRepository.todoListPreferencesFlow.asLiveData()
+
+    private val _state = MutableStateFlow(TodoState())
+
     init {
         getTodoItems()
         getTodoListPreferences()
+    }
+
+    fun onEvent(event: TodoEvent) {
+        when(event) {
+            is TodoEvent.DeleteTodo -> {
+
+            }
+            is TodoEvent.HideAddTodoDialog -> {
+                _state.update { it.copy(
+                    isAddingTodo = false
+                ) }
+            }
+            is TodoEvent.SaveTodo -> {
+
+                val itemDescription: String = _state.value.todoItemDescription
+                if (itemDescription.isBlank()) {
+                    return
+                }
+
+                val todoItem: TodoItem = TodoItem
+                    .newBuilder()
+                    .setItemId(UUID.randomUUID().toString())
+                    .setItemDescription(itemDescription).build()
+
+                viewModelScope.launch {
+                    todoItemsRepository.updateTodoItems(todoItem)
+                }
+            }
+            is TodoEvent.SetTodoDescription -> {
+                _state.update { it.copy(
+                    todoItemDescription = it.todoItemDescription
+                ) }
+            }
+            is TodoEvent.ShowAddTodoDialog ->  {
+                _state.update { it.copy(
+                    isAddingTodo = true
+                ) }
+            }
+        }
     }
 
     private fun getTodoItems() {
