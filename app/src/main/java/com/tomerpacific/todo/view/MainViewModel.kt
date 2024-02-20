f7a44271af9726e5e14a55de37a61ca45d4aa5ec
unknown <tomerpacific@gmail.com>
2024-02-20 23:00:35 +0200

    Todo adding list of items to viewmodel

--- app/src/main/java/com/tomerpacific/todo/view/MainViewModel.kt
+++ app/src/main/java/com/tomerpacific/todo/view/MainViewModel.kt
@@ -32,6 +32,7 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
     private val todoItemsRepository: TodoItemsRepository = TodoItemsRepository(application.todoItemsStore)
     private val todoListPreferencesRepository: TodoListPreferencesRepository = TodoListPreferencesRepository(application.todoListPreferencesDatastore)
     private val _state = MutableStateFlow(TodoState())
+    private var _todoItems = listOf<TodoItem>()
     val state: StateFlow<TodoState> = _state.asStateFlow()
 
     init {
@@ -64,6 +65,11 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
                 viewModelScope.launch {
                     todoItemsRepository.updateTodoItems(todoItem)
                 }
+
+                _state.update { it.copy(
+                    isAddingTodo = false,
+                    todoItemDescription = ""
+                ) }
             }
             is TodoEvent.SetTodoDescription -> {
                 _state.update { it.copy(
@@ -88,7 +94,13 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
 
     private fun getTodoItems() {
         viewModelScope.launch {
-            todoItemsRepository.fetchCachedTodoItems()
+            val todoItems = todoItemsRepository.fetchCachedTodoItems()
+            if (todoItems.itemsList.isNotEmpty()) {
+                _todoItems = todoItems.itemsList
+                _state.update { it.copy(
+                    todoItems = _todoItems
+                ) }
+            }
         }
     }
 
