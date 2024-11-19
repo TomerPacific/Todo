fd84618e5a01d7f71d3e070269f6ebb3cf4b2b67
unknown <tomerpacific@gmail.com>
2024-11-19 22:47:52 +0200

    Todo adding UI for checkbox

--- app/src/main/java/com/tomerpacific/todo/view/ui/TodoScreen.kt
+++ app/src/main/java/com/tomerpacific/todo/view/ui/TodoScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -12,6 +13,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.DismissValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -22,8 +24,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -47,6 +51,7 @@ fun TodoScreen(
     }
 
     val shouldShowDialog = remember { mutableStateOf(false) }
+    var didUserCheckToCompleteAllTodos by remember { mutableStateOf(true) }
 
     Scaffold(floatingActionButton = {
 
@@ -84,6 +89,16 @@ fun TodoScreen(
                             fontSize = 25.sp
                         )
                     }
+                }
+            }
+            item {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    Checkbox(checked = didUserCheckToCompleteAllTodos, onCheckedChange = {
+                        didUserCheckToCompleteAllTodos = it
+                    })
                 }
             }
 
